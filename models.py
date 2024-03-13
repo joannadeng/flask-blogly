8410d000264781adc31aee8e3fdb5f8973f3c7a5
Huixian Deng <huixiandeng@joannas-mbp.lan>
2024-03-12 22:55:13 -0400

    tested 3

--- models.py
+++ models.py
@@ -17,7 +17,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key = True, autoincrement=True)
     first_name = db.Column(db.Text, nullable = False)
     last_name = db.Column(db.Text, nullable = False)
-    image_url = db.Column(db.Text, default = default_image_url )
+    image_url = db.Column(db.Text, nullable = False, default = default_image_url )
 
 class Post(db.Model):
     __tablename__='posts'
